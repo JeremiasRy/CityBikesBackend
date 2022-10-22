f0ecd94c3459c5951f93dcf44a652a8fe79ecf71
JeremiasRy <ryttari.jeremias@gmail.com>
2022-10-22 15:32:45 +0300

    Exception handling.

--- CityBikesMinimalBackEnd/Api.cs
+++ CityBikesMinimalBackEnd/Api.cs
@@ -1,9 +1,12 @@
-﻿namespace CityBikesMinimalBackEnd;
+﻿using Microsoft.Extensions.Logging;
+
+namespace CityBikesMinimalBackEnd;
 
 public static class Api
 {
-    public static void ConfigureApi(this WebApplication app)
-    {
+    static readonly LoggerFactory _loggerFactory = new();
+    public static void ConfigureApi(this WebApplication app) 
+    { 
         app.MapGet("/journeys", GetJourneys);
         app.MapGet("/stations", GetStations);
         app.MapGet("/station/stats", GetStationStatistics);
@@ -11,16 +14,61 @@ public static class Api
         app.MapGet("/station/stats/departures", GetStationTop5Departures);
     }
 
-    static async Task<IResult> GetJourneys(IJourneyData data, string? date, string? durationOperator, string? distanceOperator, int? duration, int? distance, string? departureStationId, string? returnStationId, string? orderBy, string? orderDirection, int pageIndex = 1, int pageSize = 100)
+    static async Task<IResult> GetJourneys(IJourneyData data, string? date, string? durationOperator, string? distanceOperator, int? duration, int? distance, string? departureStationId, string? returnStationId, string? orderBy, string? orderDirection, int pageIndex = 1, int pageSize = 50)
     {
-        return Results.Ok(await data.GetJourneys(date, durationOperator, distanceOperator, duration, distance, departureStationId, returnStationId, orderBy, orderDirection, pageIndex, pageSize));
+        try
+        {
+            return Results.Ok(await data.GetJourneys(date, durationOperator, distanceOperator, duration, distance, departureStationId, returnStationId, orderBy, orderDirection, pageIndex, pageSize));
+
+        } catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+        
     }
 
-    static async Task<IResult> GetStations(IStationData data) => Results.Ok(await data.GetStations());
+    static async Task<IResult> GetStations(IStationData data)
+    {
+        try
+        {
+            return Results.Ok(await data.GetStations());
+        } catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }
 
-    static async Task<IResult> GetStationStatistics(IStationData data, string stationId) => Results.Ok(await data.GetStationStatistics(stationId));
+    static async Task<IResult> GetStationStatistics(IStationData data, string stationId)
+    {
+        try
+        {
+            return Results.Ok(await data.GetStationStatistics(stationId));
+        } catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
 
-    static async Task<IResult> GetStationTop5Returns(IStationData data, string stationId) => Results.Ok(await data.GetStationTop5Returns(stationId));
+    }
 
-    static async Task<IResult> GetStationTop5Departures(IStationData data, string stationId) => Results.Ok(await data.GetStationTop5Departures(stationId));
+    static async Task<IResult> GetStationTop5Returns(IStationData data, string stationId)
+    {
+        try 
+        { 
+            return Results.Ok(await data.GetStationTop5Returns(stationId)); 
+        } catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    }  
+
+    static async Task<IResult> GetStationTop5Departures(IStationData data, string stationId)
+    {
+        try
+        {
+           return Results.Ok(await data.GetStationTop5Departures(stationId));
+        } catch (Exception ex)
+        {
+            return Results.Problem(ex.Message);
+        }
+    } 
 }
--- CityBikesMinimalBackEnd/Program.cs
+++ CityBikesMinimalBackEnd/Program.cs
@@ -6,6 +6,16 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<ISqlAccess, SqlAccess>();
 builder.Services.AddSingleton<IJourneyData, JourneyData>();
 builder.Services.AddSingleton<IStationData, StationData>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000");
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,6 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.ConfigureApi();
 
